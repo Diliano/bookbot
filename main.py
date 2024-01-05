9beb4f1b4c0fa44e46c835646a9225af813547f5
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-05 14:38:52 +0000

    Added get_word_count function that returns word count of a given book text. Tested and working in main

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def main():
     frankenstein_path = "books/frankenstein.txt"
-    text = get_book_text(frankenstein_path)
-    print(text)
+    frankenstein_text = get_book_text(frankenstein_path)
+    print(frankenstein_text)
+    frankenstein_word_count = get_word_count(frankenstein_text)
+    print(frankenstein_word_count)
 
 def get_book_text(book_path):
     with open(book_path) as f:
         file_contents = f.read()
         return file_contents
+
+def get_word_count(book_text):
+    words = book_text.split()
+    word_count = len(words)
+    return word_count
 
 main()
