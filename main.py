ebb0a2f8f0c535080e730036b023fedc8a61c8cb
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-05 14:22:22 +0000

    Refactored 'with' block into get_book_text function. Tested and working in main function with a call to the function for the frankenstein book

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    frankenstein_path = "books/frankenstein.txt"
+    text = get_book_text(frankenstein_path)
+    print(text)
+
+def get_book_text(book_path):
+    with open(book_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        return file_contents
 
 main()
