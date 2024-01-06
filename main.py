e20b57f2e5f911e56121c2beba7b5ecd8c1a82f8
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-06 13:30:14 +0000

    Converted get_characters_dict function to get_letters_dict. Function checks if character isalpha() and creates a string/integer dictionary for each letter and its corresponding count, where any uppercase instances are first converted to lowercase

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 def main():
     frankenstein_path = "books/frankenstein.txt"
     frankenstein_text = get_book_text(frankenstein_path)
-    print(frankenstein_text)
     frankenstein_word_count = get_word_count(frankenstein_text)
-    print(frankenstein_word_count)
-    print(get_characters_dict(frankenstein_text))
+    frankeinstein_letters_dict = get_letters_dict(frankenstein_text)
+    print(frankeinstein_letters_dict)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -16,14 +15,16 @@ def get_word_count(book_text):
     word_count = len(words)
     return word_count
 
-def get_characters_dict(book_text):
-    characters_dict = {}
+def get_letters_dict(book_text):
+    letters_dict = {}
     lowered_book_text = book_text.lower()
     for character in lowered_book_text:
-        if character in characters_dict:
-            characters_dict[character] += 1
-        else:
-            characters_dict[character] = 1
-    return characters_dict
+        if character.isalpha():
+            letter = character
+            if letter in letters_dict:
+                letters_dict[letter] += 1
+            else:
+                letters_dict[letter] = 1
+    return letters_dict
 
 main()
