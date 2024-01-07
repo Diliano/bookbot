cefd2671314d0b1443b8746bcf0ca2949f99eca0
Dilan Patel <113666014+Diliano@users.noreply.github.com>
2024-01-07 09:00:14 +0000

    Added get_report function that creates a formatted report for a given book with its book path, word count and letters count in descending order

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def main():
     frankenstein_text = get_book_text(frankenstein_path)
     frankenstein_word_count = get_word_count(frankenstein_text)
     frankeinstein_letters_count_list = get_letters_count_list(frankenstein_text)
-    print(frankeinstein_letters_count_list)
+    frankenstein_report = get_report(frankenstein_path, frankenstein_word_count, frankeinstein_letters_count_list)
+    print(frankenstein_report)
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -28,5 +29,13 @@ def get_letters_count_list(book_text):
     letters_count_list = list(letters_dict.items())
     letters_count_list.sort(key = lambda i: i[1], reverse = True)
     return letters_count_list
+
+def get_report(book_path, word_count, letters_count_list):
+    report = f"--- Begin report of {book_path} ---\n"
+    report += f"{word_count} words found in the document\n\n"
+    for letter_count in letters_count_list:
+        report += f"The '{letter_count[0]}' character was found {letter_count[1]} times\n"
+    report += f"--- End report ---"
+    return report
      
 main()
